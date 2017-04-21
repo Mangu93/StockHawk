7aa8ee7b1537fc9a4e643311749ec41d7f067ab5
mangu93 <adrianmarinportillo@gmail.com>
2017-04-21 11:31:47 +0200

    Second deliver: graph added

--- app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
@@ -2,16 +2,36 @@ package com.udacity.stockhawk.ui;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.udacity.stockhawk.R;
 import com.udacity.stockhawk.data.Contract;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+
+import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -32,9 +52,12 @@ public class DetailActivity extends AppCompatActivity {
     TextView price;
     @BindView(R.id.change)
     TextView change;
-    @BindView(R.id.historic)
-    TextView historic;
-
+    @BindView(R.id.chart)
+    LineChart chart;
+    @BindString(R.string.stock_graph)
+    String stock;
+    @BindString(R.string.loading_graph)
+    String loading;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,14 +68,14 @@ public class DetailActivity extends AppCompatActivity {
         if (uri != null) {
             try {
                 cursor = getApplicationContext().getContentResolver().query(uri, COLUMNS, null, null, null);
-                if(cursor.moveToFirst()) {
+                if (cursor.moveToFirst()) {
                     String percentage_change = cursor.getString(Contract.Quote.POSITION_ABSOLUTE_CHANGE);
                     String cursor_symbol = cursor.getString(Contract.Quote.POSITION_SYMBOL);
                     String cursor_price = cursor.getString(Contract.Quote.POSITION_PRICE);
                     String cursor_history = cursor.getString(Contract.Quote.POSITION_HISTORY);
-                    if(!cursor_history.equals("")) {
-                        historic.setText(cursor_history);
-                        historic.setMovementMethod(new ScrollingMovementMethod());
+                    if (!cursor_history.equals("")) {
+                        chart.setNoDataText(loading);
+                        makeChart(cursor_history);
                     }
                     symbol.setText(cursor_symbol);
                     price.setText(cursor_price);
@@ -67,5 +90,89 @@ public class DetailActivity extends AppCompatActivity {
                 }
             }
         }
+    }
+
+    private void makeChart(String cursor_history) {
+        String[] history_array = cursor_history.split("\\n");
+
+        List<String> left_values = new ArrayList<>();
+        List<Entry> right_values = new ArrayList<>();
+
+        for(int index = 0; index < history_array.length; index++) {
+            String str = history_array[index];
+            long unixSeconds = Long.parseLong(str.split(",")[0]);
+            Date date = new Date(unixSeconds*1000L);
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+            sdf.setTimeZone(TimeZone.getTimeZone("America/New_York"));
+            String formattedDate = sdf.format(date);
+            left_values.add(formattedDate);
+            right_values.add(new Entry(index, Float.parseFloat(str.split(",")[1])));
+        }
+
+        final ArrayList<String> xValues = new ArrayList<>(left_values);
+        final ArrayList<Entry> yVals1 = new ArrayList<>(right_values);
+        final ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                chart.setBackgroundColor(Color.WHITE);
+                chart.setAlpha(0.9f);
+                chart.setGridBackgroundColor(Color.TRANSPARENT);
+                chart.setDrawGridBackground(true);
+
+                chart.setDrawBorders(true);
+                Description description = new Description();
+                description.setText(stock);
+                chart.setDescription(description);
+                chart.setPinchZoom(false);
+                Legend l = chart.getLegend();
+                l.setEnabled(false);
+
+                XAxis xAxis = chart.getXAxis();
+                xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+                xAxis.setDrawGridLines(false);
+                xAxis.setAxisLineColor(Color.parseColor("#20F5F5"));
+                xAxis.setTextColor(Color.parseColor("#50F5F5"));
+                xAxis.setValueFormatter(new IAxisValueFormatter() {
+                    @Override
+                    public String getFormattedValue(float value, AxisBase axis) {
+                        return xValues.get((int) value % xValues.size());
+                    }
+
+                });
+                YAxis leftAxis = chart.getAxisLeft();
+                leftAxis.setTextColor(Color.parseColor("#50F5F5"));
+                leftAxis.setDrawAxisLine(true);
+                leftAxis.setDrawZeroLine(false);
+                leftAxis.setDrawGridLines(true);
+
+                leftAxis.setGridColor(Color.parseColor("#20F5F5"));
+                leftAxis.setAxisLineColor(Color.parseColor("#20F5F5"));
+
+                chart.getAxisRight().setEnabled(false);
+                LineDataSet set1;
+
+                set1 = new LineDataSet(yVals1, "DataSet 1");
+
+                set1.setAxisDependency(YAxis.AxisDependency.LEFT);
+                set1.setColor(Color.parseColor("#50FFEB"));
+                set1.setDrawCircles(false);
+                set1.setLineWidth(1f);
+                set1.setCircleRadius(3f);
+                set1.setFillAlpha(50);
+                set1.setDrawFilled(true);
+                set1.setFillColor(Color.BLUE);
+                set1.setHighLightColor(Color.rgb(214, 217, 117));
+                set1.setDrawCircleHole(false);
+                dataSets.add(set1);
+
+                LineData datab = new LineData(dataSets);
+                datab.setDrawValues(true);
+
+                chart.setData(datab);
+
+            }
+        }, 700);
     }
 }
